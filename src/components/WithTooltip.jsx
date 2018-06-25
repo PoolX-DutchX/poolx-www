88430f43a06b5d42d33872143496b6d7ab56c821
Michael Teixeira <meixeira@gmail.com>
2018-06-26 01:47:14 +0200

    Style tooltip so icon does not impact line-height of text being 'tool-tipped'

--- src/components/WithTooltip.jsx
+++ src/components/WithTooltip.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Tooltip from '@material-ui/core/Tooltip';
 import InformationIcon from '@material-ui/icons/Info';
+import { withStyles } from '@material-ui/core/styles';
 
-function WithTooltip({children, title, placement = 'bottom-start', iconColor = 'inherit'}) {
-  return <span>
+const styles = theme => ({
+  relative: {
+    position: 'relative'
+  },
+  absolute: {
+    position: 'absolute',
+    top: '-10px',
+  },
+});
+
+function WithTooltip({children, title, placement = 'bottom-start', iconColor = 'inherit', classes}) {
+  return <span className={classes.relative}>
     {children}
     <Tooltip placement={placement} title={title}>
-      <InformationIcon color={iconColor}/>
+      <InformationIcon color={iconColor} className={classes.absolute}/>
     </Tooltip>
     </span>;
 }
 
-export default WithTooltip;
+export default withStyles(styles)(WithTooltip);
