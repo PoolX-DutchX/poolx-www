437e15e9270533ae4e2de8d19f4950c3aafb9eeb
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2019-06-12 10:38:43 +0200

    remove hamburger menu on mobile

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,72 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
-import { history } from '../lib/helpers'
 
 /**
  * The main top menu
  */
-class MainMenu extends Component {
-  constructor(props) {
-    super(props)
+const MainMenu = ({ location }) => {
+  const isHomePage = location.pathname === '/'
 
-    this.state = {
-      showMobileMenu: false,
-    }
-  }
+  return (
+    <div>
+      <nav
+        id="main-menu"
+        className={`navbar navbar-expand-lg fixed-top ${isHomePage && 'is-homepage'} `}
+      >
+        <Link className="navbar-brand" to="/">
+          {isHomePage ? (
+            <img src="/img/poolx_logo.png" width="140px" alt="PoolX logo" />
+          ) : (
+            <img src="/img/logo_white.png" width="140px" alt="PoolX logo" />
+          )}
+        </Link>
 
-  componentDidMount() {
-    // when route changes, close the menu
-    history.listen(() => this.setState({ showMobileMenu: false }))
-  }
-
-  toggleMobileMenu() {
-    this.setState({ showMobileMenu: !this.state.showMobileMenu })
-  }
-
-  render() {
-    const { showMobileMenu } = this.state
-    const { location } = this.props
-    const isHomePage = location.pathname === '/'
-
-    return (
-      <div>
-        <nav
-          id="main-menu"
-          className={`navbar navbar-expand-lg fixed-top ${
-            showMobileMenu ? 'show' : ''
-          } ${isHomePage && 'is-homepage'} `}
+        <div
+          className="collapse navbar-collapse"
+          id="navbarSupportedContent"
         >
-          <button
-            className="navbar-toggler navbar-toggler-right"
-            type="button"
-            onClick={() => this.toggleMobileMenu()}
-          >
-            <i
-              className={`navbar-toggler-icon fa ${
-                showMobileMenu ? 'fa-close' : 'fa-bars'
-              }`}
-            />
-          </button>
-          <Link className="navbar-brand" to="/">
-            {isHomePage ? (
-              <img src="/img/poolx_logo.png" width="140px" alt="PoolX logo" />
-            ) : (
-              <img src="/img/poolx_logo.png" width="140px" alt="PoolX logo" />
-            )}
-          </Link>
-
-          <div
-            className={`collapse navbar-collapse ${
-              showMobileMenu ? 'show' : ''
-            } `}
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav mr-auto" />
-          </div>
-        </nav>
-      </div>
-    )
-  }
+          <ul className="navbar-nav mr-auto" />
+        </div>
+      </nav>
+    </div>
+  )
 }
 
 export default withRouter(MainMenu)
